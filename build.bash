f1c70e3fec2590d14b6b9776c49508de19c7b9bc
Maciej Kraus <mcj.krs@gmail.com>
2025-07-26 21:37:32 +0200

    1.0.1 manifest

--- build.bash
+++ build.bash
@@ -4,4 +4,4 @@
 ./node_modules/.bin/webpack
 
 # 2. Run web-ext to package the extension
-./node_modules/.bin/web-ext build --overwrite-dest --ignore-files "tmp/" "package.json" "package-lock.json" "webpack.config.js" --ignore-files "build.bash" --ignore-files "sign.bash"
+./node_modules/.bin/web-ext build --overwrite-dest --ignore-files "spam_filter.iml" "tmp/" "package.json" "package-lock.json" "webpack.config.js" --ignore-files "build.bash" --ignore-files "sign.bash"
